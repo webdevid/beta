92e1c35b12cbdae64c252394d704846d9bbdb229
dodi hidayatullah <dodihidayatullah@gmail.com>
2016-10-01 14:40:21 +0700

    add new modal

---
+++ builder/builder_modal_widget-instagram.php
@@ -0,0 +1,151 @@
+<div class="modal fade modal-lg" id="ModalWidgetInstagram" tabindex="-1" role="dialog" aria-labelledby="ModalWidget">
+
+    <div class="modal-dialog modal-lg" role="document">
+
+        <div class="modal-content">
+
+            <div class="modal-header">
+
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+
+                <h4 class="modal-title" id="libraryModalLabel">Widget Options</h4>
+
+            </div>
+
+            <div class="modal-body">
+
+                <div class="row">
+
+                    <!--
+
+                    <div class="col-xs-12 ls-modal-button-group">
+
+                        <div class="btn-group" role="group" aria-label="">
+
+                            <button type="button" class="btn btn-default ls-modal-button" data-action="">Light</button>
+
+                            <button type="button" class="btn btn-default ls-modal-button" data-action="">Dark</button>
+
+                            <button type="button" class="btn btn-default ls-modal-button" data-action="">Alternative</button>
+
+                        </div>
+
+                    </div>-->
+
+                    <div class="col-xs-12">
+
+                        <div class="no-margin">
+
+                            <div class="form-group">
+
+                                <label for="colum-bg">Background Image</label>
+
+                                <div class="input-group">
+
+                                    <input type="text" id="widget_bg_image" class="form-control" placeholder="">
+
+                                    <span class="input-group-btn">
+
+                                        <button class="btn btn-secondary open_colum_bg" type="button" data-open="widget_bg_image">Select Image</button>
+
+                                    </span>
+
+                                </div>
+
+                            </div>
+
+                            <div class="form-group">
+
+                                <label for="background-color">Background Color</label>
+
+                                <span class="input-group-btn">
+
+                                    <input type='text' id="widget_bg_color"/>
+
+                                </span>
+
+                            </div>
+
+                            <div class="form-group">
+
+                                <label for="blockNoPadding">Padding</label>
+
+                                <div class="row">
+
+                                    <div class="col-sm-1">
+
+                                        <div class="form-group">
+
+                                            <span id="helpBlock" class="help-block font-size-12">Left</span>
+
+                                            <input type="text" class="form-control" id="widget_p_l" class="form-control" placeholder="">
+
+                                        </div>
+
+                                    </div>
+
+                                    <div class="col-sm-1">
+
+                                        <div class="form-group">
+
+                                            <span id="helpBlock" class="help-block font-size-12">Top</span>
+
+                                            <input type="text" class="form-control" id="widget_p_t" class="form-control" placeholder="">
+
+                                        </div>
+
+                                    </div>
+
+                                    <div class="col-sm-1">
+
+                                        <div class="form-group">
+
+                                            <span id="helpBlock" class="help-block font-size-12">Right</span>
+
+                                            <input type="text" class="form-control" id="widget_p_r" class="form-control" placeholder="">
+
+                                        </div>
+
+                                    </div>
+
+                                    <div class="col-sm-1">
+
+                                        <div class="form-group">
+
+                                            <span id="helpBlock" class="help-block font-size-12">Bottom</span>
+
+                                            <input type="text" class="form-control" id="widget_p_b" class="form-control" placeholder="">
+
+                                        </div>
+
+                                    </div>
+
+                                </div>
+
+                             </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div class="modal-footer">
+
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+
+                <div class="btn-group" role="group" aria-label="modal-navigation">
+
+                    <button type="button" class="btn btn-primary"  id="updateWidget">Update</button>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
